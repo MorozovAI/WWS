365441464bbd2e12987922b047291ed417df6473
MorozovAI <m407345@mail.ru>
2022-12-20 17:12:17 +0500

    first commit

---
+++ src/main/java/com/morozov/warrantywebsystem/WarrantyWebSystemApplication.java
@@ -0,0 +1,13 @@
+package com.morozov.warrantywebsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarrantyWebSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarrantyWebSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/morozov/warrantywebsystem/WarrantyWebSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morozov.warrantywebsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarrantyWebSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
